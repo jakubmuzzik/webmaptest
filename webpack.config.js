d939d4a71808e97bfc78b8750fe64da55bb909c0
Jakub Muzik <jakub.muzzik@gmail.com>
2024-01-21 20:21:33 +0100

    progress

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ module.exports = async function (env, argv) {
     const config = await createExpoWebpackConfigAsync(env, argv);
   
     config.resolve.alias['react-native-maps'] = '@teovilla/react-native-web-maps';
+    config.resolve.alias['lottie-react-native'] = 'react-native-web-lottie';
 
     return config;
 };
